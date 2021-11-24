b2254bb0456cdf4730edfc73d398479748374e08
Matheus Alves <matheus.aalves@hotmail.com>
2021-11-24 14:35:11 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
+// 1. Implemente a função getSpeciesByIds
+// Esta função é responsável pela busca das espécies de animais por id.
+// Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
+
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+function getSpeciesByIds(...ids) {
+  return data.species.filter((specie) => ids.includes(specie.id)); 
+} // Source: https://www.w3schools.com/jsref/jsref_includes.asp
 
 module.exports = getSpeciesByIds;
