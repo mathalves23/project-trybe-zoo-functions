c3004f7d2937dcfb55e8d2e15712c8fa3cd0d64d
Matheus Alves <matheus.aalves@hotmail.com>
2021-11-24 19:12:19 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstSpecie = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const oldestAnimal = data.species.find((specie) => specie.id === firstSpecie).residents.reduce((accumulator, current) => {
+    if (accumulator.age > current.age) return accumulator;
+    return current;
+  });
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
+
