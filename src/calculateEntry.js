1c572c82f2f2aacbbc5221684886536bd0c28296
Matheus Alves <matheus.aalves@hotmail.com>
2021-11-24 16:50:55 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  let senior = 0;
+  let adult = 0;
+  let child = 0;
+
+  entrants.forEach((entrant) => {
+    if (entrant.age >= 50) {
+      senior += 1;
+    } else if (entrant.age >= 18) {
+      adult += 1;
+    } else {
+      child += 1;
+    }
+  });
+  return { senior, adult, child };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  let { senior, adult, child } = countEntrants(entrants);
+  senior *= prices.senior;
+  adult *= prices.adult;
+  child *= prices.child;
+  return senior + adult + child;
 }
 
 module.exports = { calculateEntry, countEntrants };
