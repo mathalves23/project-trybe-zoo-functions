3d4823ed5fc2aae5a2da6b8c7240d2bf71df4cc9
Matheus Alves <matheus.aalves@hotmail.com>
2021-11-25 10:29:26 -0300

    Requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const employeeData = data.employees.map((employee) => {
+  const employeeInfo = {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: employee.responsibleFor.map((IdAnimal) =>
+      data.species.find((info) => info.id === IdAnimal).name),
+    locations: employee.responsibleFor.map((IdAnimal) =>
+      data.species.find((info) => info.id === IdAnimal).location),
+  };
+  return employeeInfo;
+});
+
+function getEmployeesCoverage(element = employeeData) {
+  const employeesCheck = employeeData.find((employee) =>
+    employee.fullName.includes(element.name) || employee.id === element.id);
+  if (element.name || element.id) {
+    if (employeesCheck === undefined) {
+      throw new Error('Informações inválidas');
+    }
+    return employeesCheck;
+  }
+  return element;
 }
 
 module.exports = getEmployeesCoverage;
